6fb92ae0688819a1c74b397bd766c811574332c7
Wang Yunpeng <me@beholdwild.org>
2026-02-16 12:07:53 +0900

    update with japanese search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 let data = null;
 let zhNames = [];
+let jpNames = [];
 
 fetch("./data/birds-data.json")
   .then((res) => res.json())
   .then((json) => {
     data = json;
     zhNames = Object.keys(data.zh_index);
+    jpNames = Object.keys(data.jp_index);
   });
 
 function searchZhNames(input) {
@@ -23,6 +25,29 @@ function searchZhNames(input) {
     }
   }
   return results;
+}
+
+function searchJpNames(input) {
+  if (input === "") return [];
+
+  const results = [];
+
+  for (let i = 0; i < jpNames.length; i++) {
+    if (toKatakana(jpNames[i]).includes(toKatakana(input))) {
+      results.push(jpNames[i]);
+
+      if (results.length >= 20) {
+        break;
+      }
+    }
+  }
+  return results;
+}
+
+function toKatakana(str) {
+  return str.replace(/[\u3041-\u3096]/g, function (ch) {
+    return String.fromCharCode(ch.charCodeAt(0) + 0x60);
+  });
 }
 
 function render(list) {
@@ -47,6 +72,8 @@ function showResult(entry) {
   aLink.href = entry.link;
 }
 
+const main = document.querySelector("main");
+const langSwitch = document.getElementById("lang-switch");
 const input = document.getElementById("search");
 const reset = document.getElementById("reset");
 const dropdownMenu = document.getElementById("dropdown");
@@ -58,6 +85,16 @@ const spanJp = results.querySelector(".jp");
 const spanRarity = results.querySelector(".rarity");
 const aLink = results.querySelector("a");
 
+langSwitch.addEventListener("click", () => {
+  if (main.dataset.searchlang === "zh") {
+    main.dataset.searchlang = "jp";
+    input.placeholder = "输入日语鸟名";
+  } else if (main.dataset.searchlang === "jp") {
+    main.dataset.searchlang = "zh";
+    input.placeholder = "输入中文鸟名";
+  }
+});
+
 input.addEventListener("input", (e) => {
   if (!data) return;
 
@@ -67,7 +104,13 @@ input.addEventListener("input", (e) => {
     dropdownMenu.innerHTML = "";
     return;
   }
-  const list = searchZhNames(value);
+
+  let list = [];
+  if (main.dataset.searchlang === "zh") {
+    list = searchZhNames(value);
+  } else if (main.dataset.searchlang === "jp") {
+    list = searchJpNames(value);
+  }
   if (list.length > 0) {
     dropdownMenu.classList.remove("hidden");
     render(list);
@@ -81,10 +124,18 @@ reset.addEventListener("click", () => {
 dropdownMenu.addEventListener("click", (e) => {
   if (e.target.tagName !== "SPAN") return;
 
-  const id = data.zh_index[e.target.textContent];
-  const speciesInfo = data.species[id];
+  let id;
+  if (main.dataset.searchlang === "zh") {
+    id = data.zh_index[e.target.textContent];
+  } else if (main.dataset.searchlang === "jp") {
+    id = data.jp_index[e.target.textContent];
+  }
 
-  dropdownMenu.classList.add("hidden");
-  input.value = e.target.textContent;
-  showResult(speciesInfo);
+  if (id != null) {
+    const speciesInfo = data.species[id];
+
+    dropdownMenu.classList.add("hidden");
+    input.value = e.target.textContent;
+    showResult(speciesInfo);
+  }
 });
